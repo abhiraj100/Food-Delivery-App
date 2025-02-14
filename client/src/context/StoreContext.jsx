2dbbefb21f9d3b7b4ac72519d68a62037be82ee6
Abhiraj Yadav <abhiraj100right@gmail.com>
2025-02-15 03:40:28 +0530

    change client backend url

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -8,7 +8,8 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
-  const url = "http://localhost:4000";
+  // const url = "http://localhost:4000";
+  const url = "https://food-delivery-app-backend-b1ln.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
